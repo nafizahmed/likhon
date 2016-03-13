94bd73e5b46cf71b970403fb34f3e2680b980c46
MD NAFIZUR RAHMAN <MD NAFIZUR RAHMAN>
2016-03-13 16:21:03 -0400

    first repo

---
+++ bangla/src/cricket/Hot.java
@@ -0,0 +1,23 @@
+package cricket;
+
+public class Hot {
+	
+	 String  a= "bangladesh";
+
+	public static void main ( String [] args){
+		
+		int[] r ={1,2,3,4,5};
+		
+		//char [] a2=a.toCharArray();	
+		
+		//System.out.println(a2[1]);
+		//char [] m=a.toCharArray();
+		//System.out.println(m[2]);
+		//System.out.println(a.indexOf(3));
+		
+		
+		System.out.println(r[0]);
+	}
+
+
+}
